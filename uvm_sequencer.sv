c4771825eca7e9683cf3814753c01c2f731e04ee
rr1verif <131348131+rr1verif@users.noreply.github.com>
2023-04-21 22:19:08 +0530

    Update uvm_sequencer.sv

--- uvm_sequencer.sv
+++ uvm_sequencer.sv
@@ -1,1 +1,11 @@
+class uvm_test_sequencer extends uvm_sequencer;
+  
+  //Factory registration
+  `uvm_components_utils(uvm_test_sequencer)
 
+  //New constructor
+  function new(string name = "uvm_test_sequencer", uvm_component parent = null);
+    super.new(name,parent);
+  endfunction
+  
+endclass
