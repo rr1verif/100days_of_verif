5e75c1228bd2537f6a887ba929ef198ef7f253bc
rr1verif <131348131+rr1verif@users.noreply.github.com>
2023-04-21 21:43:37 +0530

    Update uvm_driver.sv

--- uvm_driver.sv
+++ uvm_driver.sv
@@ -4,5 +4,7 @@ class uvm_test_driver extends uvm_driver;
   
   //New constructor
   function new (string name = "uvm_test_driver", uvm_component parent = null);
-    super.this(name);
+    super.new(name);
+    
+    //Add the code for driving input to the DUT
   endfunction
