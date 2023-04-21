a38c2cd04fde1cfe11dc105e5686dbb2b6d5906c
rr1verif <131348131+rr1verif@users.noreply.github.com>
2023-04-21 21:41:57 +0530

    Update uvm_driver.sv

--- uvm_driver.sv
+++ uvm_driver.sv
@@ -1,1 +1,8 @@
-
+class uvm_test_driver extends uvm_driver;
+  //Fatory
+  `uvm_components_utils(uvm_test_driver);
+  
+  //New constructor
+  function new (string name = "uvm_test_driver", uvm_component parent = null);
+    super.this(name);
+  endfunction
