452c688e8119539fbd082961fda566607597d345
rr1verif <131348131+rr1verif@users.noreply.github.com>
2023-04-27 23:27:42 +0530

    Update uvm_monitor.sv

--- uvm_monitor.sv
+++ uvm_monitor.sv
@@ -1,1 +1,14 @@
-
+class test_mon extends uvm_monitor;
+  
+  //Factory
+  `uvm_components_utils(test_mon);
+  
+  //New constructor
+  function new (string name = "test_mon" , uvm_component parent = null);
+    super.new(name,parent);
+    //Create analysis port handle
+  endfunction 
+  
+  //
+  
+endclass : test_mon
