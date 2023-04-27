34be24bf20496836e48a05a0f8743b8c44a10d85
rr1verif <131348131+rr1verif@users.noreply.github.com>
2023-04-28 02:39:06 +0530

    Update uvm_checker.sv

--- uvm_checker.sv
+++ uvm_checker.sv
@@ -3,6 +3,8 @@ class uvm_test_checker extends uvm_component; //Check whether checker are uvm_co
   `uvm_analysis_imp_decl(_from_mon_to_chkr) ;
   
   `uvm_analysis_import #(mux_sequence) from_mon_to_chkr;
+  //Add code for analysis fifo 
+  `tlm_analysis_fifo #(mux_sequence) from_mon_to_chkr_fifo;
   //Factory
   `uvm_components_utils(uvm_test_checker);
   
@@ -15,13 +17,18 @@ class uvm_test_checker extends uvm_component; //Check whether checker are uvm_co
   virtual function void build_phase(uvm_phase phase);
     //Can we define handle for analysis import here
     from_mon_to_chkr = new("analysis_import",this);
+    from_mon_to_chk_fifo = new("from_mon_to_chkr_fifo",this);
   endfunction
   
-  function void write #(mux_sequence seq1);
+  function void write #(mux_sequence seq1); //Analysis import write function
   endfunction
   
   task run_phase(uvm_phase phase);
     //Add code for seq1 processing here
+    forever begin //{
+      mux_sequence seq2;
+      from_mon_to_chkr_fifo.get(seq2)
+    end //}
   endtask
     
 endclass
