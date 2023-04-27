1e5dc62e0089e739166a0026c7203d73c2ec986d
rr1verif <131348131+rr1verif@users.noreply.github.com>
2023-04-28 02:28:50 +0530

    Update uvm_env.sv

--- uvm_env.sv
+++ uvm_env.sv
@@ -1,1 +1,23 @@
-
+class uvm_test_env extends uvm_env;
+  
+  `uvm_component_utils(uvm_test_env);
+  
+  function new (string name = "uvm_test_env" , uvm_component parent = null);
+    super.new(name,parent);
+  endfunction
+  
+  function void build_phase (uvm_phase phase);
+    super.build_phase(phase);
+    //Add code here
+  endfunction
+  
+  function void connect_phase (uvm_phase phase);
+    super.connect_phase(phase);
+    //Add connection here
+  endfunction
+  
+  task run_phase (uvm_phase phase)
+    //Add Simulation related code here
+  endtask
+  
+endclass
