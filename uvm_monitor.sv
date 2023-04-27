9ab61cc4a935f2a9498a6f270a8dae6d145a5082
rr1verif <131348131+rr1verif@users.noreply.github.com>
2023-04-28 02:00:31 +0530

    Added analysis port mon_to_chkr pkt info

--- uvm_monitor.sv
+++ uvm_monitor.sv
@@ -1,6 +1,6 @@
 class test_mon extends uvm_monitor;
   
-  `uvm_ananlysis_port#(mux_sequence) to_drv_from_mon;
+  `uvm_ananlysis_port #(mux_sequence) mon_to_chkr;
   //Factory
   `uvm_components_utils(test_mon);
   
@@ -8,8 +8,27 @@ class test_mon extends uvm_monitor;
   function new (string name = "test_mon" , uvm_component parent = null);
     super.new(name,parent);
     //Create analysis port handle
+    ap = new("analysis_port", this);
   endfunction 
   
-  //
+  //Logic for run_phase
+  task run_phase(uvm_phase phase);
+    //Define a virtual interface handle named vif
+    
+    //Define a new object of mux_sequence type
+    mux_sequence mux_seq_1;
+    //forever loop
+    forever begin //{
+      if (vif.clk) begin //{
+        //Assign interface signals to the object fields
+        mux_seq_1.input1 = vif.input1;
+        mux_seq_1.input2 = vif.input2;
+        mux_seq_1.sel1   = vif.sel1;
+        //Call the write function and push the packet
+        ap.write(mux_seq_1);
+        `uvm_info(get_type_name(),$sformatf("Packet written to checker is %p", mux _seq_1),UVM_LOW);
+      end //}
+    end //}
+  endtask: run_phase
   
 endclass : test_mon
