8a6cba5b41a3ba2094761e747ac35f7d0d6ba4fb
rr1verif <131348131+rr1verif@users.noreply.github.com>
2023-04-30 21:19:29 +0530

    Update uvm_driver.sv

--- uvm_driver.sv
+++ uvm_driver.sv
@@ -1,6 +1,7 @@
-class uvm_test_driver extends uvm_driver;
+class uvm_test_driver extends uvm_driver #(mux_sequence);
   //Fatory
   `uvm_components_utils(uvm_test_driver);
+  uvm_test_if vif_drv;
   
   //New constructor
   function new (string name = "uvm_test_driver", uvm_component parent = null);
@@ -8,3 +9,17 @@ class uvm_test_driver extends uvm_driver;
     
     //Add the code for driving input to the DUT
   endfunction
+  
+  function void build_phase(uvm_phase phase);
+    if(!uvm_config_db(virtual uvm_test_if)::get(this,"","uvm_test_if",vif_drv);
+       `uvm_fatal("VIF","Error have you set this interface");
+  endfunction
+    
+  task run_phase(uvm_phase phase);
+  endtask : run_phase
+
+  task drive_seq();
+  endtask : drive_seq
+       
+  
+endclass 
